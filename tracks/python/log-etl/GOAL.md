e5955fd4e437a925143d6e083840a7e783ccebc4
ryancocuzzo <rcocuzzo8@gmail.com>
2026-02-13 14:30:15 -0500

    log etl uv project scaffolding, input processing, input file json parsing

--- tracks/python/log-etl/GOAL.md
+++ tracks/python/log-etl/GOAL.md
@@ -6,9 +6,15 @@ The goal here is re-familiarizing with the Python language. I haven't written a 
 
 A python program that..
 
-- [ ] Accepts a log file path and an output file path via system input
-- [ ] Ingests log data
+- [X] Accepts a log file path and an output file path via system input
+- [X] Ingests log data
 - [ ] Computes aggregates
+    - [ ] Total revenue
+    - [ ] Average order value
+    - [ ] Top 3 SKUs by revenue
+- [ ] Computes rolling information
+    - [ ] Revenue by hour
+    - [ ] Ingrediate burn rate (Chicken vs Rice)
 - [ ] Outputs aggregates to an output file
 - [ ] Uses modular ETL patterns
 
--- tracks/python/log-etl/main.py
+++ tracks/python/log-etl/main.py
@@ -1,5 +1,38 @@
 
+import orjson
+import sys
+
+def load_input_file_json(path: str) -> list[dict]:
+    try:
+        with open(path, "r") as f:
+            # trade-off is right now we're storing all the items into memory
+            log_lines = []
+            for line in f:
+                if line == "" or line == "\n":
+                    continue
+                data = orjson.loads(line)
+                log_lines.append(data)
+            return log_lines
+    except FileNotFoundError as err:
+        print(f"Could not find file {path}")
+        raise err
+    except PermissionError as err:
+        print(f'Could not access file {path}')
+        raise err
+
 def main():
-    print("hello world")
+    try:
+        # 1. get input and output file paths
+        input_file = input("Specify the input file: ")
+        # output_file = input("Specify the output file: ")
+
+        data = load_input_file_json(input_file)
+
+        print(data)
+
+    except KeyboardInterrupt:
+        print("\nUser signaled to exit application.")
+    finally:
+        print("Exiting applicaiton.")
 
 main()
